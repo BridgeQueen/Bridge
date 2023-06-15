87c3e5faa6a95b5a138eaa48717708687eeb3344
CK Wong <ckwong@ualberta.ca>
2023-06-15 01:56:29 -0600

    Added loading page but have to fix npm as it can't start

---
+++ front_end/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import SidebarItem from "./SidebarItem"
+import items from "../data/sidebar.json"
+
+export default function Sidebar(){
+    return (
+        
+        <div className="sidebar">
+            {items.map((item, index) => <SidebarItem key={index} item={item} />)}
+        </div>
+        
+    )
+}
