b4d3d81c7d0d562e651a64b496dc1f48baff5978
CK Wong <ckwong@ualberta.ca>
2023-06-15 01:58:20 -0600

    Add App.js

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Sidebar />
+      <div className="container">
+        <h1 className="title">Welcome to Bridge Queen!</h1>
+        <p className="info">lorem ipsum</p>
+        <button className="btn">Explore Now</button>
+      </div>
     </div>
   );
 }
